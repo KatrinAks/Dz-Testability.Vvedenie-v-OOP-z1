fed841f394b0b2a40a7acb310ba97460e0dfd2a6
kat test <e_aksenova80@mail.ru>
2023-03-24 18:22:26 +0300

    Main.java change

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,2 +1,13 @@
 public class BonusMilesService {
+
+    public int calculate(int price){
+        int oneMile = 20;
+        int miles = 0;
+
+        if (price>0) {
+            miles = price / oneMile;
+        }
+        return miles;
+    }
 }
+
